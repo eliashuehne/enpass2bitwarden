c7c2ff79212acf837625f41c18d83a6c8aaf8fe0
eliashuehne <6222871+eliashuehne@users.noreply.github.com>
2026-08-24 17:18:11 +0000

    formula: point at v1.0.0 release assets

--- Formula/enpass2bitwarden.rb
+++ Formula/enpass2bitwarden.rb
@@ -1,32 +1,36 @@
 class Enpass2bitwarden < Formula
   desc "Migrate an Enpass v6 vault to Bitwarden/Vaultwarden (passkeys + attachments included)"
   homepage "https://github.com/eliashuehne/enpass2bitwarden"
-  version "1.1.1"
-  license "MIT"
+  version "1.0.0"
 
-  livecheck do
-    url :stable
-    strategy :github_latest
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/eliashuehne/enpass2bitwarden/releases/download/v1.0.0/enpass2bitwarden-darwin-arm64"
+      sha256 "c1922d1133c9957d7121b6d652aa7c8d29a76be82cd32b9ac82813004e08bb92"
+    end
   end
 
-  if OS.mac?
-    url "https://github.com/eliashuehne/enpass2bitwarden/releases/download/v#{version}/enpass2bitwarden-darwin-arm64"
-    sha256 "b84c9d9f36c2af7d3608afe361d863ba5d8361864456606b86f07973b54cef2f"
-  elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/eliashuehne/enpass2bitwarden/releases/download/v#{version}/enpass2bitwarden-linux-amd64"
-    sha256 "a3c3c3fddc0d313110d6cef4860152f169e9cf6efa636fde95abbc52b8a9eebd"
-  elsif OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/eliashuehne/enpass2bitwarden/releases/download/v#{version}/enpass2bitwarden-linux-arm64"
-    sha256 "cbd141758257569abd073e007e3d7056aa55e239ddd13fe149b6d097e5bbe859"
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/eliashuehne/enpass2bitwarden/releases/download/v1.0.0/enpass2bitwarden-linux-amd64"
+      sha256 "f92d7d2d458af762ae7e7f63dcf7c7b9500dcfeb548e5cd07f7e0fadc53dccb8"
+    elsif Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+      raise UnsupportedArchitectureException.new("arm32 not supported")
+    else
+      url "https://github.com/eliashuehne/enpass2bitwarden/releases/download/v1.0.0/enpass2bitwarden-linux-arm64"
+      sha256 "d6fe25bd54407bcfe56390186b546f852f4c937e7824cd74e23da3efc970f34b"
+    end
   end
 
   def install
-    bin.install Dir["enpass2bitwarden-*"].first => "enpass2bitwarden"
+    bin.install "enpass2bitwarden-darwin-arm64" => "enpass2bitwarden" if OS.mac?
+    bin.install "enpass2bitwarden-linux-amd64" => "enpass2bitwarden" if OS.linux? && Hardware::CPU.intel?
+    bin.install "enpass2bitwarden-linux-arm64" => "enpass2bitwarden" if OS.linux? && Hardware::CPU.arm?
   end
 
   def caveats
     <<~EOS
-      You also need the Bitwarden CLI:
+      Requires the Bitwarden CLI ('bw') for the import and attach steps:
         brew install bitwarden-cli
     EOS
   end
